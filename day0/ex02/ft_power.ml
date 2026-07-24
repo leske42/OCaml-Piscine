758a21d0db97d259127e50f2343599cd71b0e710
leske42 <mhuszar@student.42vienna.com>
2026-07-24 20:15:36 +0000

    fix(day0/ex02): correct 0th power

--- day0/ex02/ft_power.ml
+++ day0/ex02/ft_power.ml
@@ -1,4 +1,4 @@
 let rec ft_power a b =
-  if b > 1 then a * ft_power a (b - 1) else a
+  if b = 0 then 1 else if b > 1 then a * ft_power a (b - 1) else a
 
-let () = print_int (ft_power 3 1)
+let () = Printf.printf "%d\n%d\n" (ft_power 3 1) (ft_power 3 0)
