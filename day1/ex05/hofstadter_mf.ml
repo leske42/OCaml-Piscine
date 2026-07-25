56ae1e4f77ed599c7bd41edb8442c4a5b70fe31b
leske42 <mhuszar@student.42vienna.com>
2026-07-25 12:54:32 +0000

    feat(day1): add hofstadter_mf

---
+++ day1/ex05/hofstadter_mf.ml
@@ -0,0 +1,7 @@
+let rec hfs_f n =
+  if n = 0 then 1 else if n > 0 then n - hfs_m (hfs_f (n - 1)) else -1
+
+and hfs_m n =
+  if n = 0 then 0 else if n > 0 then n - hfs_f (hfs_m (n - 1)) else -1
+
+let () = Printf.printf "%d\n" (hfs_f 4)
