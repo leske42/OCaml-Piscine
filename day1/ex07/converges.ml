b1f6cb566d02889b2fd240a8e972c4bdf8f273d4
leske42 <mhuszar@student.42vienna.com>
2026-07-25 17:49:44 +0000

    refactor: more converges version

--- day1/ex07/converges.ml
+++ day1/ex07/converges.ml
@@ -1,12 +1,21 @@
 let rec iter f x n =
-  if n < 0 then -1 else if n = 0 then x else f (iter f x (n - 1))
+  if n < 0 then -1 else if n = 0 then x else iter f (f x) (n - 1)
 
-let converges f x n = if iter f x n = iter f x (n + 1) then true else false
+let converges f x n =
+  let hehe = iter f x n in
+  if hehe = f hehe then true else false
+  
+let rec iter f x n prev =
+  if x = prev then true else if n = 0 then false else iter f (f x) (n - 1) x
+
+let converges f x n =
+  if n < 0 then false else
+  iter f (f x) n x
 
 let () =
   if converges (( * ) 2) 2 5 then Printf.printf "true\n"
   else Printf.printf "false\n";
   if converges (fun x -> x / 2) 2 3 then Printf.printf "true\n"
   else Printf.printf "false\n";
-  if converges (fun x -> x / 2) 2 2 then Printf.printf "true\n"
+  if converges (fun x -> x / 2) 2 200000000 then Printf.printf "true\n"
   else Printf.printf "false\n"
