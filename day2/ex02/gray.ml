bf5dc20fbf6fdd3e5c98b4c9a1d25a8c31572aa3
leske42 <mhuszar@student.42vienna.com>
2026-07-26 14:12:41 +0000

    refactor(day2): make gray shorter

--- day2/ex02/gray.ml
+++ day2/ex02/gray.ml
@@ -2,18 +2,11 @@ let rec applied_to lst str =
   match lst with
   | [] -> []
   | x :: rest -> (str ^ x) :: applied_to rest str
-(*this will be reversed...*)
 
 let rec gray n =
-  let construct n ~forward:dir =
     match n with
-    | 1 when dir = true -> [ "0" ]
-    | 1 when dir = false -> [ "1" ]
-    | x when dir = true -> "0" |> applied_to (gray (x - 1))
-    | x when dir = false -> "1" |> applied_to (gray (x - 1))
-    | _ -> [] (*why is this needed?*)
-  in
-  construct n ~forward:true @ List.rev (construct n ~forward:false)
+    | 1 -> [ "0"; "1" ]
+    | _ -> ("0" |> applied_to (gray (n - 1))) @ List.rev ("1" |> applied_to (gray (n - 1)))
 
 (* let () =
   List.iter (Printf.printf "%d ") (crossover [ 1; 2; 3 ] [ 3; 4; 5; 1 ]);
