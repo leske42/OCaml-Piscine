f19f61a291c9808923a68e36090036f7112eff2d
leske42 <mhuszar@student.42vienna.com>
2026-07-29 16:51:20 +0000

    feat(day3): add Color

---
+++ day3/ex00/main.ml
@@ -0,0 +1,3 @@
+let () = Printf.printf "The string of type Spade is: %s (short) and %s (long)\n" (Color.toString Spade) (Color.toStringVerbose Spade);
+  Printf.printf "The string of type Heart is: %s (short) and %s (long)\n" (Color.toString Heart) (Color.toStringVerbose Heart)
+  
